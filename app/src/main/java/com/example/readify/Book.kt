1d1748a918b110fece7a27d6b6d566433582480e
prayfortherain <prayfortherain@yandex.ru>
2024-01-21 00:27:35 +0300

    change cards

---
+++ app/src/main/java/com/example/readify/Book.kt
@@ -0,0 +1,11 @@
+package com.example.readify
+
+data class Book (
+    val realiseDate: String,
+    val title: String,
+    val author: String,
+    val genre: String,
+    val isAvailable: Boolean,
+    val articleNumber : String,
+    val description : String
+)
--- app/src/main/java/com/example/readify/HomeScreen.kt
+++ app/src/main/java/com/example/readify/HomeScreen.kt
@@ -1,20 +1,26 @@
 package com.example.readify
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
@@ -23,6 +29,7 @@ import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -35,8 +42,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -106,7 +114,7 @@ fun HomeScreen() {
         ) { index ->
             Box(
                 modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.TopCenter
             ) {
                 BookList()
                 //Text(text = tabList[index].title)
@@ -117,14 +125,8 @@ fun HomeScreen() {
 @Composable
 fun BookList() {
     val books = listOf(
-        BookInfo("Книга 1", "Автор 1", "Жанр 1", true),
-        BookInfo("Книга 2", "Автор 2", "Жанр 2", false),
-        BookInfo("Книга 2", "Автор 2", "Жанр 2", false),
-        BookInfo("Книга 2", "Автор 2", "Жанр 2", false),
-        BookInfo("Книга 2", "Автор 2", "Жанр 2", false),
-        BookInfo("Книга 2", "Автор 2", "Жанр 2", false),
-        BookInfo("Книга 2", "Автор 2", "Жанр 2", false),
-        BookInfo("Книга 2", "Автор 2", "Жанр 2", false),
+        Book("01-01-2024", "Книга 1", "Автор 1", "Жанр 1", true, "123456", "Автобиография небезиствестного Автора 1, что прославился своими бестселлерами, такими как Книга 2 и Книга 3"),
+        Book("01-01-2024", "Книга 2", "Автор 1", "Жанр 1", false, "789456", "Увлекательное произведение Автора 1, которое необходимо прочитать каждой целеустремленной личности")
     )
     LazyColumn {
         items(books) { book ->
@@ -132,28 +134,32 @@ fun BookList() {
         }
     }
 }
-data class BookInfo(
-    val title: String,
-    val author: String,
-    val genre: String,
-    val isAvailable: Boolean
 
-)
 @Composable
-fun BookItem(book: BookInfo) {
+fun BookItem(book: Book) {
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.background)
             .padding(16.dp)
-            .background(color = Color.Magenta)
-            .padding(16.dp)
-            .border(1.dp, MaterialTheme.colorScheme.secondary, shape = MaterialTheme.shapes.medium)
-            .clickable { }
+            .border(1.dp, MaterialTheme.colorScheme.secondary, shape = RoundedCornerShape(8.dp))
+            .clickable {
+                /*TODO NAVIGATION*/
+            }
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceAround
         ) {
+            Image(
+                bitmap = ImageBitmap.imageResource(R.drawable.img), /*TODO PICTURES*/
+                contentDescription = null,
+                contentScale = ContentScale.Inside,
+                modifier = Modifier
+                    .height(107.dp)
+                    .width(80.dp)
+            )
             Column(
                 modifier = Modifier
                     .weight(1f)
@@ -167,14 +173,10 @@ fun BookItem(book: BookInfo) {
                     color = if (book.isAvailable) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.error
                 )
             }
-
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Outlined.FavoriteBorder, contentDescription = null) /*TODO FAVORITES*/
+            }
+            Spacer(modifier = Modifier.width(7.dp))
         }
     }
 }
-
-data class TabItem(
-    val title: String,
-    val unselectedIcon: ImageVector,
-    val selectedIcon: ImageVector
-)
-
