8d9b6c2554bee011d32e02ed866168e8f87b9944
marie <122727409+mouhodokenai@users.noreply.github.com>
2024-01-30 13:53:29 +0300

    VM added

---
+++ app/src/main/java/com/example/readify/RegisterVm.kt
@@ -0,0 +1,15 @@
+package com.example.readify
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class RegisterVm(private val repository: NetworkRepository): ViewModel() {
+    val success = MutableLiveData<Boolean>()
+    fun register(name: String, email: String, password: String){
+        viewModelScope.launch {
+            repository.register(name, email, password)
+        }
+    }
+}
